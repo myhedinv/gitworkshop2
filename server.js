7d8b29fb19e3298138c6b26da33a435b642d7d6f
myhedinv <myhedin.vuciterna@student.uni-pr.edu>
2024-03-29 15:05:31 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,42 @@
+const net = require('net');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function handleUserInput() {
+    rl.question('', (userInput) => {  
+        client.write(userInput);
+        handleUserInput(); // Repeat the prompt
+    });
+  }
+
+const host = 'localhost';
+const port = 3000;
+const client = new net.Socket();
+
+client.connect(port, host, () => {
+    console.log('Connected to server');
+    handleUserInput;
+});
+
+client.on('data', (data) => {
+    console.log(`${data}`);
+    const resp = data.toString();
+    if (resp.startsWith("/")) { // is a command
+        if (resp.startsWith("/exit")) {
+            client.destroy();
+        }
+    }
+});
+
+client.on('close', () => {
+    console.log('Connection closed');
+    client.destroy();
+});
+
+client.on('error', (err) => {
+    console.log(`Error: ${err}`)
+});
